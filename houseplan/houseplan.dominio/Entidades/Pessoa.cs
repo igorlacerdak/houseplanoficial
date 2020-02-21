b1066dc78fa0f65c50a67698d4053f79112af246
Igor Lacerda <igorlacerdasantos@hotmail.com>
2020-02-20 21:37:14 -0300

    Criando entidades no dominio

---
+++ houseplan/houseplan.dominio/Entidades/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace houseplan.dominio.Entidades
+{
+    public class Pessoa
+    {
+        public int Cod_Pessoa { get; set; }
+        public String Nome { get; set; }
+    }
+}
